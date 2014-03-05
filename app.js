ab115f7643426d410d97f57eb29544fa3846f689
Jd1681 <jeremy.duprey@gmail.com>
2014-03-05 13:59:58 -0500

    fdsa

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
 
 var userInput = +prompt("Enter a number");
 
-evalInteger(userInput);
+while(userInput != NaN) {
+	userInput = +prompt("Enter a number");
+	}
 
 for(var counter = 1; counter <= userInput; counter++) {
 	if (counter % 3 == 0) {
